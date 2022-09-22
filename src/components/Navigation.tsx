ad46b7b175a4f6ebe4dfc77e180125d41ec6ac7b
skharitonov <skharitonov@sitronics.com>
2022-09-22 14:16:45 +0300

    first commit

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+export const Navigation = () => {
+    return (
+        <nav className="flex justify-between items-center h-[50px] px-5 shadow-md bg-gray-500 text-white">
+            <h3 className="font-bold">Github Search</h3>
+
+            <span>
+                <Link to="/" className="mr-2 hover:underline">Home</Link>
+                <Link to="/favorites" className="hover:underline">Favorites</Link>
+            </span>
+        </nav>
+    );
+};
---
+++ src/pages/FavoritesPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const FavoritesPage = () => {
+    return (
+        <div>
+            Favorites
+        </div>
+    );
+};
